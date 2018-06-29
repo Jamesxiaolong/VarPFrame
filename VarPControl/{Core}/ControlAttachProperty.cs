65b6d2d2ef5d7336caac27e29b6de1d77d4a5771
Jamesxiaolong <2438746951@qq.com>
2018-06-29 14:31:25 +0800

    1.1

---
+++ VarPControl/{Core}/ControlAttachProperty.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace VarPControl
+{
+    /// <summary>
+    /// 公共附加属性类
+    /// </summary>
+    public static class ControlAttachProperty
+    {
+
+        #region FocusBorderBrush 焦点边框色,输入控件
+        public static readonly DependencyProperty FocusBorderBrushProperty = DependencyProperty.RegisterAttached(
+            "FocusBorderBrush", typeof(Brush), typeof(ControlAttachProperty), new FrameworkPropertyMetadata(null));
+        public static void SetFocusBorderBrush(DependencyObject element, Brush value)
+        {
+            element.SetValue(FocusBorderBrushProperty,value);
+        }
+        public static Brush GetFocusBorderBrush(DependencyObject element)
+        {
+            return (Brush)element.GetValue(FocusBorderBrushProperty);
+        }
+        #endregion
+
+
+    }
+
+}
